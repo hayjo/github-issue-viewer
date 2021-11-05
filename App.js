9a7a261bb4eca8c5df02547328101e4020a08612
Hyun Jo <hay.hyunjo@gmail.com>
2021-11-05 15:50:13 +0900

    Initialize react-native

---
+++ App.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type { Element } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { enableScreens } from 'react-native-screens';
+import Icon from 'react-native-vector-icons/Octicons';
+
+import Home from './Components/Home';
+
+Icon.loadFont();
+enableScreens(true);
+
+const Stack = createNativeStackNavigator();
+
+const App: () => Element = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
