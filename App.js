60a25e11f01d669b5e25497941c1d26555032be0
Hyun Jo <hay.hyunjo@gmail.com>
2021-11-06 01:37:19 +0900

    Add: search and select repo
    Add: Search, RepoCard, SnackBar, Home, Error components
    Add: RepoContext

--- App.js
+++ App.js
@@ -6,14 +6,17 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import type { Element } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { enableScreens } from 'react-native-screens';
 import Icon from 'react-native-vector-icons/Octicons';
 
+import { RepoContext } from './context';
 import Home from './Components/Home';
+import Error from './Components/Error';
+import { LIMIT } from './constants';
 
 Icon.loadFont();
 enableScreens(true);
@@ -21,12 +24,53 @@ enableScreens(true);
 const Stack = createNativeStackNavigator();
 
 const App: () => Element = () => {
+  const [repoList, setRepoList] = useState([]);
+  const [needNotice, setNeedNotice] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const notifyRepoLimit = () => setNeedNotice(true);
+  const handleSelectRepo = repoId => {
+    setRepoList(prevList => {
+      if (prevList.includes(repoId)) {
+        setNeedNotice(false);
+
+        return prevList.filter(id => id !== repoId);
+      }
+
+      if (prevList.length >= LIMIT.REPO_COUNT) {
+        setNeedNotice(true);
+        notifyRepoLimit();
+
+        return prevList;
+      }
+
+      return [...prevList, repoId];
+    });
+  };
+  const handleNoticeClick = () => setNeedNotice(false);
+  const notifyError = err => {
+    console.log(err);
+    setHasError(true);
+  };
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <RepoContext.Provider
+      value={{
+        selectedRepoList: repoList,
+        onSelectRepo: handleSelectRepo,
+        needNotice,
+        onNoticeClick: handleNoticeClick,
+        notifyError,
+      }}>
+      {hasError ? (
+        <Error />
+      ) : (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Home" component={Home} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      )}
+    </RepoContext.Provider>
   );
 };
 
---
+++ Components/Search.js
@@ -0,0 +1,148 @@
+import React, { useContext, useState, useMemo } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  ActivityIndicator,
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+} from 'react-native';
+import type { Element } from 'react';
+import Icon from 'react-native-vector-icons/Octicons';
+import RepoCard from './RepoCard';
+import SnackBar from './SnackBar';
+import { RepoContext } from '../context';
+import { searchRepoByQuery } from '../api';
+import { COLORS, MESSAGE } from '../constants';
+
+const Search: () => Element = ({ onCancel }) => {
+  const {
+    selectedRepoList,
+    onSelectRepo,
+    needNotice,
+    onNoticeClick,
+    notifyError,
+  } = useContext(RepoContext);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [resultCount, setResultCount] = useState(0);
+  const [repoList, setRepoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onChangeSearch = query => setSearchQuery(query);
+
+  const searchRepositories = useMemo(
+    () => async query => {
+      if (!query) {
+        return;
+      }
+
+      setIsLoading(true);
+
+      try {
+        const searchResult = await searchRepoByQuery(query);
+        const { totalCount, items } = searchResult;
+
+        setRepoList(items);
+        setResultCount(totalCount);
+      } catch (err) {
+        notifyError(err);
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [notifyError],
+  );
+
+  return (
+    <SafeAreaView>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View>
+          <View style={styles.searchBarContainer}>
+            <View style={styles.searchBar}>
+              <Icon name="search" style={styles.searchIcon} size={16} />
+              <TextInput
+                placeholder="Search Repository"
+                onChangeText={onChangeSearch}
+                value={searchQuery}
+                onSubmitEditing={() => searchRepositories(searchQuery)}
+              />
+            </View>
+            <Button title="Cancel" onPress={onCancel} />
+          </View>
+          {isLoading ? (
+            <ActivityIndicator />
+          ) : (
+            <View style={styles.cardSection}>
+              <View style={styles.resultCountContainer}>
+                <Text style={styles.resultCount}>
+                  {resultCount.toLocaleString()} repository results
+                </Text>
+              </View>
+              {repoList &&
+                repoList.map(({ id, full_name: fullName, description }) => (
+                  <RepoCard
+                    key={id}
+                    title={fullName}
+                    description={description}
+                    onPress={() => onSelectRepo(id)}
+                    selected={selectedRepoList.includes(id)}
+                  />
+                ))}
+            </View>
+          )}
+        </View>
+      </ScrollView>
+      {needNotice && (
+        <SnackBar onPress={() => onNoticeClick()} style={styles.noticeSnackBar}>
+          <Text style={styles.noticeText}>{MESSAGE.MAX_REPO_COUNT}</Text>
+        </SnackBar>
+      )}
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  searchBar: {
+    flexDirection: 'row',
+    flex: 1,
+    marginHorizontal: 4,
+    marginVertical: 2,
+    backgroundColor: COLORS.SEARCH_BACKGROUND,
+    borderRadius: 8,
+    overflow: 'hidden',
+    paddingRight: 4,
+  },
+  searchBarContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 12,
+  },
+  searchIcon: {
+    alignSelf: 'center',
+    marginHorizontal: 8,
+    color: COLORS.ICON,
+  },
+  cardSection: {
+    paddingHorizontal: 16,
+  },
+  resultCountContainer: {
+    marginVertical: 16,
+  },
+  resultCount: {
+    fontSize: 16,
+    color: COLORS.SUBTITLE,
+  },
+  noticeSnackBar: {
+    padding: 10,
+    backgroundColor: COLORS.NOTICE,
+    borderRadius: 4,
+  },
+  noticeText: {
+    color: '#ffffff',
+    textAlign: 'center',
+  },
+});
+
+export default Search;
