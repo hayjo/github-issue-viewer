cd5bd9e1103f27e7516be481682c50b76cae8ea3
Hyun Jo <hay.hyunjo@gmail.com>
2021-11-09 13:34:56 +0900

    Add: infinite scroll in repo search
    Modify: replace ScrollView as FlatList in Search

--- api/index.js
+++ api/index.js
@@ -1,17 +1,19 @@
 import { mockSearchRepoResult, mockIssues } from './mockData';
-import { MESSAGE } from '../constants';
+import { MESSAGE, LIMIT } from '../constants';
 
 const useMock = false;
 const MOCK_DELAY = 500;
 
-async function searchRepoByQuery(query) {
+async function searchRepoByQuery(query, page) {
   if (useMock) {
+    await new Promise(resolve => setTimeout(resolve, MOCK_DELAY));
+
     return new Promise(resolve => resolve(mockSearchRepoResult));
   }
 
   try {
     const response = await fetch(
-      `https://api.github.com/search/repositories?q=${query}&sort=stars&order=desc`,
+      `https://api.github.com/search/repositories?q=${query}&sort=stars&order=desc&per_page=${LIMIT.SEARCH_REPO_PER_PAGE}&page=${page}`,
     );
 
     if (!response.ok) {
