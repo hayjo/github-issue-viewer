5e60870fb30fe7bea6ad0a68f13c17ddbf21a6e8
Hyun Jo <hay.hyunjo@gmail.com>
2021-11-10 23:12:47 +0900

    Test: App, Repository, Search snapshot tests

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,14 +1,12 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
+import renderer from 'react-test-renderer';
 import App from '../App';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+describe('App component test', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<App />).toJSON();
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+    expect(tree).toMatchSnapshot();
+  });
 });
